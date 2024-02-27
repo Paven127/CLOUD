02047a9422e66c9b901aa9fe3d1ba7cb126cf033
Paven127 <109147408+Paven127@users.noreply.github.com>
2024-02-27 14:46:29 +0200

    Email Register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Data;
+using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -9,12 +14,19 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Undisclosed_Shop.Models;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+using Undisclosed_Shop.Controllers;
+
+
 
 namespace Undisclosed_Shop.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
@@ -188,6 +200,142 @@ namespace Undisclosed_Shop.Controllers
                     return RedirectToAction("Index", "Products");
                 }
                 AddErrors(result);
+
+                string SName = model.Name.ToString();
+
+
+                PdfDocument document = new PdfDocument();
+                //Adds page settings
+                document.PageSettings.Orientation = PdfPageOrientation.Portrait;
+                document.PageSettings.Margins.All = 50;
+                //Adds a page to the document
+                PdfPage page = document.Pages.Add();
+                PdfGraphics graphics = page.Graphics;
+                //Loads the image from disk
+                PdfImage image = PdfImage.FromFile(Server.MapPath("~/Content/Images/Evetech.jpeg")); 
+                RectangleF bounds = new RectangleF(10, 10, 200, 200);
+                //Draws the image to the PDF page
+                page.Graphics.DrawImage(image, bounds);
+                PdfBrush solidBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
+                bounds = new RectangleF(0, bounds.Bottom + 90, graphics.ClientSize.Width, 30);
+                //Draws a rectangle to place the heading in that region.
+                graphics.DrawRectangle(solidBrush, bounds);
+                //Creates a font for adding the heading in the page
+                PdfFont subHeadingFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 14);
+                //Creates a text element to add the invoice number
+                PdfTextElement element = new PdfTextElement("Invoice " + model.Name.ToString() + " for" + " " + model.Email, subHeadingFont);
+                element.Brush = PdfBrushes.White;
+
+                //Draws the heading on the page
+                PdfLayoutResult res = element.Draw(page, new PointF(10, bounds.Top + 8));
+                string currentDate = "Date Purchased " + DateTime.Now;
+                //Measures the width of the text to place it in the correct location
+                SizeF textSize = subHeadingFont.MeasureString(currentDate);
+                PointF textPosition = new PointF(graphics.ClientSize.Width - textSize.Width - 10, res.Bounds.Y);
+                //Draws the date by using DrawString method
+                graphics.DrawString(currentDate, subHeadingFont, element.Brush, textPosition);
+                PdfFont timesRoman = new PdfStandardFont(PdfFontFamily.TimesRoman, 10);
+                //Creates text elements to add the address and draw it to the page.
+          //      element = new PdfTextElement("Bill To " + sale.Address.ToString() + ", " + sale.City + ", " + sale.State, timesRoman);
+                element.Brush = new PdfSolidBrush(new PdfColor(126, 155, 203));
+                res = element.Draw(page, new PointF(10, res.Bounds.Bottom + 25));
+          //      element = new PdfTextElement("Total Price R" + sale.SaleTotal.ToString(), timesRoman);
+                element.Brush = new PdfSolidBrush(new PdfColor(126, 155, 203));
+                res = element.Draw(page, new PointF(10, res.Bounds.Bottom + 25));
+                PdfPen linePen = new PdfPen(new PdfColor(126, 151, 173), 0.70f);
+                PointF startPoint = new PointF(0, res.Bounds.Bottom + 3);
+                PointF endPoint = new PointF(graphics.ClientSize.Width, res.Bounds.Bottom + 3);
+                //Draws a line at the bottom of the address
+                graphics.DrawLine(linePen, startPoint, endPoint);
+
+                //Creates the datasource for the table
+                DataTable invoiceDetails = new DataTable();
+
+                //Add columns to the DataTable
+            //    invoiceDetails.Columns.Add("Products Name");
+             //   invoiceDetails.Columns.Add("Quantity");
+              //  invoiceDetails.Columns.Add("Price");
+
+                //Add rows to the DataTable
+              //  foreach (var item in sale.SaleDetails.Where(m => m.SaleId == sale.SaleId))
+               // {
+               //     invoiceDetails.Rows.Add(new object[] { item.Products.ProductName, item.Quantity, item.Products.ProductPrice });
+               // }
+
+
+                //Creates text elements to add the address and draw it to the page.
+
+
+
+                //Creates a PDF grid
+                PdfGrid grid = new PdfGrid();
+                //Adds the data source
+                grid.DataSource = invoiceDetails;
+                //Creates the grid cell styles
+                PdfGridCellStyle cellStyle = new PdfGridCellStyle();
+                cellStyle.Borders.All = PdfPens.White;
+                PdfGridRow header = grid.Headers[0];
+                //Creates the header style
+                PdfGridCellStyle headerStyle = new PdfGridCellStyle();
+                headerStyle.Borders.All = new PdfPen(new PdfColor(126, 151, 173));
+                headerStyle.BackgroundBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
+                headerStyle.TextBrush = PdfBrushes.White;
+                headerStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 14f, PdfFontStyle.Regular);
+
+                //Adds cell customizations
+                for (int i = 0; i < header.Cells.Count; i++)
+                {
+                    if (i == 0 || i == 1)
+                        header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Left, PdfVerticalAlignment.Middle);
+                    else
+                        header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Right, PdfVerticalAlignment.Middle);
+                }
+
+                //Applies the header style
+                header.ApplyStyle(headerStyle);
+                cellStyle.Borders.Bottom = new PdfPen(new PdfColor(217, 217, 217), 0.70f);
+                cellStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 12f);
+                cellStyle.TextBrush = new PdfSolidBrush(new PdfColor(131, 130, 136));
+                //Creates the layout format for grid
+                PdfGridLayoutFormat layoutFormat = new PdfGridLayoutFormat();
+                // Creates layout format settings to allow the table pagination
+                layoutFormat.Layout = PdfLayoutType.Paginate;
+                //Draws the grid to the PDF page.
+                PdfGridLayoutResult gridResult = grid.Draw(page, new RectangleF(new PointF(0, res.Bounds.Bottom + 40), new SizeF(graphics.ClientSize.Width, graphics.ClientSize.Height - 100)), layoutFormat);
+
+                MemoryStream outputStream = new MemoryStream();
+                document.Save(outputStream);
+                outputStream.Position = 0;
+
+                var invoicePdf = new System.Net.Mail.Attachment(outputStream, System.Net.Mime.MediaTypeNames.Application.Pdf);
+                string docname = "Invoice.pdf";
+                invoicePdf.ContentDisposition.FileName = docname;
+
+                MailMessage mail = new MailMessage();
+                string emailTo = model.Email;
+                MailAddress from = new MailAddress("HotelListVX@gmail.com");
+                mail.From = from;
+                mail.Subject = "Your invoice for order number #" + model.Email;
+                mail.Body = "Dear Customer" + model.Name + " find your invoice in the attached PDF document.";
+                mail.To.Add(emailTo);
+
+                mail.Attachments.Add(invoicePdf);
+
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                NetworkCredential networkCredential = new NetworkCredential("HotelListVX@gmail.com", "ujzzmzrxomafbwkb");
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = networkCredential;
+                smtp.Port = 587;
+                smtp.Send(mail);
+                //Clean-up.
+                //Close the document.
+                document.Close(true);
+                //Dispose of email.
+                mail.Dispose();
+
             }
 
             // If we got this far, something failed, redisplay form
